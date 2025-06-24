3dcd7cb74322c9409b68614530bb8434697419a8
Frédéric Simonis <simonisfrederic@gmail.com>
2025-06-24 14:36:24 +0200

    Integrate config-check
    Closes #2

--- precicecli/native.py
+++ precicecli/native.py
@@ -1,9 +1,14 @@
 import argparse
 import subprocess
+import pathlib
+import sys
+
+import preciceconfigcheck.cli
 
 def makeCheckParser(_):
     parser = argparse.ArgumentParser(add_help=False, description="Validates configuration files using the preCICE tooling API.")
-    parser.add_argument("infile")
+    parser.add_argument("infile", type=pathlib.Path)
+    parser.add_argument("-d", "--debug", action="store_true", help="Enable debug output in the configuration checker.")
     parser.add_argument("participant", nargs="?")
     parser.add_argument("size", nargs="?")
     return parser
@@ -38,22 +43,25 @@ def runDoc(ns):
 
 
 def runCheck(ns):
+    path = ns.infile
+    if not path.is_file():
+        print(f"The given file {path} doesn't exist.", file=sys.stderr, flush=True)
+        return 1
+
     # Frist validate
-    args = ["precice-config-validate", ns.infile]
+    args = ["precice-config-validate", str(path)]
     if ns.participant:
         args.append(ns.participant)
         if ns.size:
             args.append(ns.size)
 
     try:
-        ret = subprocess.run(args)
-        return 1
+        subprocess.run(args)
     except subprocess.CalledProcessError as e:
         return e.returncode
     except FileNotFoundError:
-        print("precice-config-doc wasn't found. Please install preCICE and add it to your PATH.", file=sys.stderr, flush=True)
-        return 1
+        print("precice-config-validate wasn't found, hence the validation will be skipped. This may lead to crashes in the configuration checker.", file=sys.stderr)
+        print("Please install preCICE and add it to your PATH for best results.", file=sys.stderr)
 
     # Then check
-    # TODO
-    return 0
+    return preciceconfigcheck.cli.runCheck(path, ns.debug)
